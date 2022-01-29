6c9fb5c9320986cfba94af45ab318178568dd99b
Patrick <patrick.lie9@gmail.com>
2022-01-29 22:18:39 +0100

    removing old code

--- js/interactions.js
+++
@@ -1,8 +0,0 @@
-let moreInfoButton = document.getElementById('moreInfoButton');
-moreInfoButton.addEventListener('click', () => {
-
-    // Scroll to a certain element
-    document.querySelector('.contentAbout').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
