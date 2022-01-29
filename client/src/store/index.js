2433f192a7a1af1b49751e5c2ee519b3376650ad
Patrick <patrick.lie9@gmail.com>
2022-01-29 23:54:23 +0100

    setting up store and vuex

---
+++ client/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+console.log("hmm?", vuex);
+
+export default createStore({
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    modules: {
+        
+    }
+});
