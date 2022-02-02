3d15f9874261a6e2dc1bcd7b53c3afd8ed6e6418
Patrick <patrick.lie9@gmail.com>
2022-02-02 22:54:15 +0100

    adding enemys to the shooter game

--- client/src/components/Shooter.vue
+++ client/src/components/Shooter.vue
@@ -16,11 +16,12 @@ export default {
     app: null,
     game: null,
     world: new World(),
+    spawnTimer: 0,
   }),
   computed: {},
   methods: {
     start() {
-      this.world.wo["test"] = new GO(
+      this.world.wo["bg"] = new GO(
         this.PIXI,
         this.app,
         assets.background,
@@ -29,7 +30,17 @@ export default {
         this.app.screen.width,
         this.app.screen.height
       );
-
+/*
+      this.world.wo["bround"] = new GO(
+        this.PIXI,
+        this.app,
+        assets.foreground,
+        this.app.screen.width / 2,
+        this.app.screen.height / 2,
+        this.app.screen.width,
+        this.app.screen.height
+      );
+*/
       this.world.wo["crosshair"] = new GO(
         this.PIXI,
         this.app,
@@ -39,26 +50,78 @@ export default {
         100,
         100
       );
-
-      console.log(this.app.screen.width);
-      //console.log(this.app.renderer.plugins.interaction.mouseOverRenderer);
-      //console.log(this.app.renderer.plugins.interaction.mouse.global.x);
     },
     tick(delta) {
       if (this.app.renderer.plugins.interaction.mouseOverRenderer) {
-        this.world.wo.crosshair.entity.x = this.app.renderer.plugins.interaction.mouse.global.x;
-        this.world.wo.crosshair.entity.y = this.app.renderer.plugins.interaction.mouse.global.y;
+        this.world.wo.crosshair.entity.x =
+          this.app.renderer.plugins.interaction.mouse.global.x;
+        this.world.wo.crosshair.entity.y =
+          this.app.renderer.plugins.interaction.mouse.global.y;
       }
+      this.handleSpawner();
+      this.enemyHandler();
+    },
+    handleSpawner() {
+      if (this.spawnTimer > 100) {
+        this.spawnTimer = 0;
+
+        let randInt = this.generateRandomInt();
+        let enemyName = `enemy${randInt}`;
+        this.world.wo[enemyName] = new GO(
+          this.PIXI,
+          this.app,
+          assets.enemys.enemy1,
+          this.generateRandomIntInRange(10, this.app.screen.width - 10),
+          this.app.screen.height + 50,
+          100,
+          100
+        );
+        this.world.wo[enemyName].tag = "enemy";
+
+        this.world.wo.crosshair.entity.parent.removeChild(this.world.wo.crosshair.entity);
+        this.world.wo["crosshair"] = new GO(
+          this.PIXI,
+          this.app,
+          assets.crosshair,
+          this.app.renderer.plugins.interaction.mouse.global.x,
+          this.app.renderer.plugins.interaction.mouse.global.y,
+          100,
+          100
+        );
+      } else {
+        this.spawnTimer++;
+      }
+    },
+    enemyHandler() {
+      for (const [key, value] of Object.entries(this.world.wo)) {
+        if (value.tag && value.tag == "enemy") {
+          value.entity.y -=1;
+          if (value.entity.y < -50) {
+            console.log("Jaa", this.world.wo[`${key}`]);
+            this.world.wo[`${key}`].entity.parent.removeChild(this.world.wo[`${key}`].entity);
+          }
+        }
+      }
+    },
+    generateRandomInt() {
+      return Math.floor(Math.random(1000) * 100);
+    },
+    generateRandomIntInRange(min, max) {
+      return Math.random() * (max - min) + min;
     }
   },
   mounted: function () {
-    this.game = new Game(document.getElementById("game"), (PIXI, app) => {
-      this.app = app;
-      this.PIXI = PIXI;
-      this.start();
-    }, (delta) => {
-      this.tick(delta);
-    });
+    this.game = new Game(
+      document.getElementById("game"),
+      (PIXI, app) => {
+        this.app = app;
+        this.PIXI = PIXI;
+        this.start();
+      },
+      (delta) => {
+        this.tick(delta);
+      }
+    );
   },
   beforeUnmount: function () {},
 };
--- client/src/importreg/shooterassetreg.js
+++ client/src/importreg/shooterassetreg.js
@@ -1,7 +1,17 @@
 import crosshair from "@/assets/gameAssets/blink/crosshair.png";
-import background from "@/assets/gameAssets/blink/country-platform-back.png"
+import background from "@/assets/gameAssets/blink/country-platform-back.png";
+import foreground from "@/assets/gameAssets/blink/country-platform-tiles-example.png";
+import enemy1 from "@/assets/gameAssets/blink/enemys/hawk.png";
+import enemy2 from "@/assets/gameAssets/blink/enemys/imp.png";
+import enemy3 from "@/assets/gameAssets/blink/enemys/jellyfish.png";
 
 export default {
     crosshair,
-    background
+    background,
+    foreground,
+    enemys: {
+        enemy1,
+        enemy2,
+        enemy3
+    }
 }
